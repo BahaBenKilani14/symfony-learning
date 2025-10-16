68320acb2359eb38e13c362cc8272d3bdebf5ca9
bah benkilani <baha.benkilani11@gmail.com>
2025-10-16 06:51:31 +0100

    Author CRUD added

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Author;
+
+use App\Form\AuthorType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -25,19 +31,59 @@ final class AuthorController extends AbstractController
     }
 
     #[Route('/listauthors', name: 'listauthors')]
-    public function listAuthors(): Response
+    public function listAuthors(ManagerRegistry $doctrine): Response
     {
-        $authors = array(
-            array('id' => 1, 'picture' => '/images/images/Victor-Hugo.jpg','username' => 'Victor Hugo', 'email' =>
-            'victor.hugo@gmail.com ', 'nb_books' => 100),
-            array('id' => 2, 'picture' => '/images/images/william-shakespeare.jpg','username' => ' William Shakespeare', 'email' =>
-            ' william.shakespeare@gmail.com', 'nb_books' => 200 ),
-            array('id' => 3, 'picture' => '/images/images/Taha_Hussein.jpg','username' => 'Taha Hussein', 'email' =>
-            'taha.hussein@gmail.com', 'nb_books' => 300 ),
-        );
-
+        $authorRepository = $doctrine->getRepository('App\Entity\Author');
+        $authors = $authorRepository->findAll();
         return $this->render('author/list.html.twig', [
             'authors' => $authors,
+        ]);
+    }
+
+    #[Route('/deleteauth/{id}', name: 'deleteAuth')]
+    public function deleteAuth($id, ManagerRegistry $doctrine): Response
+    {
+        $authorRepository = $doctrine->getRepository('App\Entity\Author');
+        $authors = $authorRepository->find($id);
+        $em = $doctrine->getManager();
+        $em->remove($authors);
+        $em->flush();
+        return $this->redirectToRoute('listauthors');
+    }
+
+    #[Route('/addauthor', name: 'addAuthor')]
+    public function addAuthor(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $author= new Author;
+        $em = $doctrine->getManager();
+        $form=$this->createForm(AuthorType::class,$author);
+        $form->add('Ajouter',SubmitType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()){
+            $em->persist($author);
+            $em->flush();
+            return $this->redirectToRoute('listauthors');
+        }
+        return $this->render('author/addauth.html.twig',[
+            'formauth'=>$form->createView()
+        ]);
+    }
+
+    #[Route('/updateauthor/{id}', name: 'updateauth')]
+    public function updateauth($id, Request $request, ManagerRegistry $doctrine): Response
+    {
+        $authorRepository = $doctrine->getRepository('App\Entity\Author');
+        $author = $authorRepository->find($id);
+        $em = $doctrine->getManager();
+        $form=$this->createForm(AuthorType::class,$author);
+        $form->add('Modifier',SubmitType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()){
+            $em->flush();
+            return $this->redirectToRoute('listauthors');
+        }
+        return $this->render('author/addauth.html.twig',[
+            'formauth'=>$form->createView()
         ]);
     }
 }
